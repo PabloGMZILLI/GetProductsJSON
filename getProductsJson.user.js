a7a2a004783442da0f32e65dd30ed06293865c20
Pablo Maronezi Zilli <pablo.zilli@compasso.com.br>
2019-05-22 10:58:20 -0300

    Fix links in .user

--- getProductsJson.user.js
+++ getProductsJson.user.js
@@ -3,10 +3,9 @@
 // @version      0.0.1
 // @description  Create products ids json.
 // @author       Pablo Maronezi, Fernando Basso
-// @homepageURL  https://github.com/PabloGMZILLI/Get-products-JSON
-// @source       https://github.com/PabloGMZILLI/Get-products-JSON
+// @source       https://github.com/PabloGMZILLI/GetProductsJSON
 // @match        ************************ADICIONE AQUI O LINK DO DRUPAL DESTA FORMA => *://drupal.com/*  ******************
 // @match        ************************PODEM TER QUANTOS LINKS QUISER, SÓ CLONAR ESSA LINHA  **************************
-// @require      https://raw.githubusercontent.com/PabloGMZILLI/Get-products-JSON/master/main.js
+// @require      https://raw.githubusercontent.com/PabloGMZILLI/GetProductsJSON/master/main.js
 // @grant        none
 // ==/UserScript==
